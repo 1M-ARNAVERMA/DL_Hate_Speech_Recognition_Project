06481c98c5fc6675c57157f9ad677a1fb873be5c
ARNAV VERMA <ARNAV.23FE10CSE00191@muj.manipal.edu>
2026-03-12 00:08:47 +0530

    Update preprocess.py

---
+++ preprocessing/preprocess.py
@@ -0,0 +1,20 @@
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)      # remove URLs
+    text = re.sub(r"@\w+", "", text)         # remove mentions
+    text = re.sub(r"#", "", text)            # remove hashtags
+    text = re.sub(r"[^a-zA-Z\s]", "", text)  # remove special characters
+    return text
+
+banned_words = [
+    "word1",
+    "word2",
+    "word3"
+]
+
+def contains_banned_words(text):
+    return any(word in text for word in banned_words)
+
+df = df[~df["clean_text"].apply(contains_banned_words)]
